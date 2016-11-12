36264c22e0ebaed5d8230bfe66b3ab486776a59d
Ron Gross <chiefninjaofficer@gmail.com>
2016-11-12 21:40:00 +0200

    Navbar works

--- main.js
+++ main.js
@@ -6,3 +6,47 @@ function resizeRows() {
 
 $(document).ready(resizeRows);
 $(window).resize(resizeRows);
+
+
+// Change active navbar element on scroll
+// https://jsfiddle.net/cse_tushar/Dxtyu/141/
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.nav li').each(function () {
+        var currItem = $(this);
+        var currLink = $(this).find("a");
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav ul li a').removeClass("active");
+            currItem.addClass("active");
+        }
+        else{
+            currItem.removeClass("active");
+        }
+    });
+}
